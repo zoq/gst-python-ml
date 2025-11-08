684b2ba47f32bac68f71087e17fd107cb48dac80
Aaron Boxer <aaron.boxer@collabora.com>
2025-11-08 07:23:44 -0500

    format converter: clean up imports

--- plugins/python/utils/format_converter.py
+++ plugins/python/utils/format_converter.py
@@ -16,17 +16,11 @@
 # Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
 # Boston, MA 02110-1301, USA.
 
-from utils.runtime_utils import runtime_check_gstreamer_version
 import gi
 import numpy as np
-from video_transform import VideoTransform
 
 gi.require_version("Gst", "1.0")
-gi.require_version("GstBase", "1.0")
-gi.require_version("GstVideo", "1.0")
-gi.require_version("GstAnalytics", "1.0")
-gi.require_version("GLib", "2.0")
-from gi.repository import Gst, GstAnalytics, GstVideo, GObject, GLib  # noqa: E402
+from gi.repository import Gst  # noqa: E402
 
 
 class FormatConverter:

568b04c058544c9925c5f9d201477b5b2665dd41
Aaron Boxer <aaron.boxer@collabora.com>
2025-11-23 09:40:55 -0500

    demucs: SAMPLE_RATE is class variable

--- plugins/python/demucs.py
+++ plugins/python/demucs.py
@@ -121,7 +121,7 @@ class Demucs(BaseSeparate):
     def do_separate(self, audio_data):
         # audio_data: np.float32, shape (length,) at SAMPLE_RATE Hz mono
         engine = self.engine
-        original_rate = SAMPLE_RATE
+        original_rate = self.SAMPLE_RATE
 
         # Resample to model's sample rate (44100 Hz) if necessary
         if original_rate != engine.sample_rate:
